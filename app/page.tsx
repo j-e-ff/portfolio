8e85ed5c38a23b50e0a7c60616595fa727be0634
j-e-ff <118487175+j-e-ff@users.noreply.github.com>
2025-10-07 17:09:58 -0700

    added scrolling when user presses the buttons on the navbar, also add the icons on the bottom left for communication

--- app/page.tsx
+++ app/page.tsx
@@ -101,30 +101,187 @@ export default function Home() {
     };
   }, []);
 
+  const scrollToSection = (id: string) => {
+    const section = document.getElementById(id);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  function openEmailClient(
+    recipient: string,
+    subject: string = " ",
+    body: string = " "
+  ): void {
+    const encodedSubject = encodeURIComponent(subject);
+    const encodedBody = encodeURIComponent(body);
+
+    // construct mailto URL
+    const mailtoUrl = `mailto:${recipient}?subject=${encodedSubject}&body=${encodedBody}`;
+
+    // oepn window
+    window.open(mailtoUrl, "_blank");
+  }
+
   return (
-    <div className="font-sans grid grid-rows-[40px_1fr_20px] items-center justify-center min-h-screen">
+    <div className="font-sans grid grid-rows-1 items-center justify-center min-h-screen">
       {/* Navbar at top of page */}
       <header className="hidden sm:flex sm:flex-row gap-4 backdrop-blur-3xl fixed top-0 justify-center z-50  h-10 w-full ">
-        <button className="brightness-50 hover:brightness-100">Home </button>
-        <button className="brightness-50 hover:brightness-100">Projects</button>
-        <button className="brightness-50 hover:brightness-100">
+        <button
+          className="brightness-50 hover:brightness-100"
+          onClick={() => scrollToSection("about")}
+        >
+          Home{" "}
+        </button>
+        <button
+          className="brightness-50 hover:brightness-100"
+          onClick={() => scrollToSection("projects")}
+        >
+          Projects
+        </button>
+        <button
+          className="brightness-50 hover:brightness-100"
+          onClick={() => scrollToSection("certifications")}
+        >
           Certifications
         </button>
-        <button className="brightness-50 hover:brightness-100">
+        <button
+          className="brightness-50 hover:brightness-100"
+          onClick={() => scrollToSection("education")}
+        >
           Education
         </button>
       </header>
 
-      <header className="sm:hidden flex w-full fixed top-6 right-4 justify-end ">
+      <header className="sm:hidden flex w-full fixed top-6 right-4 justify-end z-50">
         <button>Menu</button>
       </header>
 
       {/* Links to GitHub, LinkedIn, email; Displayed on side of page (bottom left)*/}
-      <nav></nav>
+      <nav className="flex flex-col fixed bottom-12 left-8 gap-4">
+        {/* GITHUB */}
+        <div>
+          <Link
+            href="https://github.com/j-e-ff"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <svg
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-[30px] h-[30px] stroke-black dark:stroke-white hover:stroke-[#5FED83] transition-colors duration-300 cursor-pointer"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M12 2C6.475 2 2 6.475 2 12C2 16.425 4.8625 20.1625 8.8375 21.4875C9.3375 21.575 9.525 21.275 9.525 21.0125C9.525 20.775 9.5125 19.9875 9.5125 19.15C7 19.6125 6.35 18.5375 6.15 17.975C6.0375 17.6875 5.55 16.8 5.125 16.5625C4.775 16.375 4.275 15.9125 5.1125 15.9C5.9 15.8875 6.4625 16.625 6.65 16.925C7.55 18.4375 8.9875 18.0125 9.5625 17.75C9.65 17.1 9.9125 16.6625 10.2 16.4125C7.975 16.1625 5.65 15.3 5.65 11.475C5.65 10.3875 6.0375 9.4875 6.675 8.7875C6.575 8.5375 6.225 7.5125 6.775 6.1375C6.775 6.1375 7.6125 5.875 9.525 7.1625C10.325 6.9375 11.175 6.825 12.025 6.825C12.875 6.825 13.725 6.9375 14.525 7.1625C16.4375 5.8625 17.275 6.1375 17.275 6.1375C17.825 7.5125 17.475 8.5375 17.375 8.7875C18.0125 9.4875 18.4 10.375 18.4 11.475C18.4 15.3125 16.0625 16.1625 13.8375 16.4125C14.2 16.725 14.5125 17.325 14.5125 18.2625C14.5125 19.6 14.5 20.675 14.5 21.0125C14.5 21.275 14.6875 21.5875 15.1875 21.4875C17.1727 20.8173 18.8977 19.5415 20.1198 17.8395C21.3419 16.1376 21.9995 14.0953 22 12C22 6.475 17.525 2 12 2Z"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </Link>
+        </div>
+
+        {/* LINKEDIN */}
+        <div className="group">
+          <Link
+            href="https://linkedin.com/in/jeffrey-rodas-018665250"
+            rel="noopener noreferer"
+            target="_blank"
+          >
+            <svg
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-[30px] h-[30px] stroke-black dark:stroke-white group-hover:stroke-[#0077B5] transition-colors duration-300 cursor-pointer"
+            >
+              <path
+                d="M18 22V15C18 13.8954 17.1046 13 16 13C14.8954 13 14 13.8954 14 15V22H10"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M10 22V15C10 11.6863 12.6863 9 16 9C19.3137 9 22 11.6863 22 15V22H18"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <rect
+                x="3"
+                y="9"
+                width="4"
+                height="13"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <circle
+                cx="5"
+                cy="4"
+                r="2"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </Link>
+        </div>
+
+        {/* EMAIL */}
+        <div
+          className="group"
+          onClick={() =>
+            window.open(
+              `mailto:${"rodasjeffrey@gmail.com"}?subject=${""}&body=${""}`,
+              "_blank"
+            )
+          }
+        >
+          <div>
+            <svg
+              viewBox="0 0 192 192"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              className="w-[30px] h-[30px] group-hover:cursor-pointer "
+            >
+              {/* Define the gradient */}
+              <defs>
+                <linearGradient
+                  id="gmailGradient"
+                  x1="0"
+                  y1="0"
+                  x2="192"
+                  y2="192"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop offset="35%" stopColor="#EA4335" /> {/* Red */}
+                  <stop offset="35%" stopColor="#34A853" /> {/* Green */}
+                  <stop offset="50%" stopColor="#4285F4" /> {/* Blue */}
+                  <stop offset="75%" stopColor="#FBBC05" /> {/* Yellow */}
+                </linearGradient>
+              </defs>
+
+              {/* Apply gradient on hover */}
+              <path
+                stroke="black"
+                strokeWidth="12"
+                strokeLinejoin="round"
+                d="M22 57.265V142c0 5.523 4.477 10 10 10h24V95.056l40 30.278 40-30.278V152h24c5.523 0 10-4.477 10-10V57.265c0-13.233-15.15-20.746-25.684-12.736L96 81.265 47.684 44.53C37.15 36.519 22 44.032 22 57.265Z"
+                className="transition-all duration-500 group-hover:stroke-[url(#gmailGradient)] dark:stroke-white"
+              />
+            </svg>
+          </div>
+        </div>
+      </nav>
 
       <main className="flex flex-col gap-[32px] overflow-y-auto w-full">
         {/* About Me Section */}
-        <div className="w-full min-h-screen flex flex-col gap-20 sm:flex-row justify-center items-center sm:gap-40 relative overflow-hidden">
+        <div
+          id="about"
+          className="w-full min-h-screen flex flex-col gap-8 sm:flex-row justify-center items-center sm:gap-40 relative overflow-hidden"
+        >
           <div className="text">
             <p className="text-3xl font-bold">Software Engineer</p>
             <p className="text-2xl font-semibold">Front-End</p>
@@ -171,13 +328,17 @@ export default function Home() {
         </div>
 
         {/* Projects Section */}
-        <div className="w-full flex flex-col gap-20 min-h-screen">
+        <div
+          id="projects"
+          className="w-full flex flex-col gap-20 min-h-screen scroll-mt-12"
+        >
           <div className="w-full flex flex-col justify-center items-center gap-6 ">
             <p className="text-2xl font-bold sticky">Projects</p>
             <p>Current projects deployed</p>
           </div>
           {/* Cards displaying the projects */}
-          <div className="felx flex-col sm:grid sm:grid-cols-2 sm:gap-4 sm:gap-y-12 w-full">
+          <div className="grid grid-cols-1 sm:grid-cols-2 sm:gap-4 gap-y-12 sm:gap-y-0 w-full sm:h-220">
+            {/* umedia */}
             <div className="observe group relative bg-[url(/umedia-home.png)] bg-cover mx-auto flex w-100 h-80 sm:w-85 sm:h-80 md:w-100 md:h-90 lg:w-120 lg:h-100 xl:w-120 sm:gap-x-4 rounded-xl ">
               <Link
                 href="https://umedia.rodasjeffrey.com"
@@ -249,13 +410,13 @@ export default function Home() {
                 </div>
               </Link>
             </div>
-
-            <Link
-              href="https://cinemasite.rodasjeffrey.com"
-              rel="noopener noreferer"
-              target="_blank"
-            >
-              <div className="observe group relative bg-[url(/cinemasite.png)] bg-cover mx-auto flex w-100 h-80 sm:w-85 sm:h-80 md:w-100 md:h-90 lg:w-120 lg:h-100  gap-x-4 rounded-xl ">
+            {/* cinemasite */}
+            <div className="observe group relative bg-[url(/cinemasite.png)] bg-cover mx-auto flex w-100 h-80 sm:w-85 sm:h-80 md:w-100 md:h-90 lg:w-120 lg:h-100  gap-x-4 rounded-xl ">
+              <Link
+                href="https://cinemasite.rodasjeffrey.com"
+                rel="noopener noreferer"
+                target="_blank"
+              >
                 <div className="absolute inset-0 hover:bg-black/80 text-center flex flex-col gap-15 p-6 rounded-xl">
                   <div className="gap-10 flex flex-col items-center">
                     <p className="text-white opacity-0 group-hover:opacity-100 brightness-200 text-xl mt-4">
@@ -305,15 +466,15 @@ export default function Home() {
                     </div>
                   </div>
                 </div>
-              </div>
-            </Link>
-
-            <Link
-              href="https://weather.rodasjeffrey.com"
-              rel="noopener noreferer"
-              target="_blank"
-            >
-              <div className="observe group relative bg-[url(/weather-app.png)] bg-cover mx-auto flex w-100 h-80 sm:w-85 sm:h-80 md:w-100 md:h-90 lg:w-120 lg:h-100 gap-x-4 rounded-xl ">
+              </Link>
+            </div>
+            {/* weather */}
+            <div className="observe group relative bg-[url(/weather-app.png)] bg-cover mx-auto flex w-100 h-80 sm:w-85 sm:h-80 md:w-100 md:h-90 lg:w-120 lg:h-100 gap-x-4 rounded-xl ">
+              <Link
+                href="https://weather.rodasjeffrey.com"
+                rel="noopener noreferer"
+                target="_blank"
+              >
                 <div className="absolute inset-0 hover:bg-black/80 text-center flex flex-col gap-15 p-6 rounded-xl">
                   <div className="gap-10 flex flex-col items-center">
                     <p className="text-white opacity-0 group-hover:opacity-100 brightness-200 text-xl ">
@@ -345,12 +506,15 @@ export default function Home() {
                     </div>
                   </div>
                 </div>
-              </div>
-            </Link>
+              </Link>
+            </div>
           </div>
         </div>
         {/* Certifications Section */}
-        <div className="w-full flex flex-col items-center gap-20 min-h-screen justify-start pt-6">
+        <div
+          id="certifications"
+          className="w-full flex flex-col items-center gap-20 min-h-screen justify-center  pt-4"
+        >
           <p className="text-3xl font-bold">Certifications</p>
           {/* Google Certification */}
           <div className="flex flex-row items-center gap-10 lg:gap-15 sm:w-135">
@@ -379,7 +543,7 @@ export default function Home() {
             </section>
           </div>
           {/* Basics of Scrum */}
-          <div className="flex flex-row justify-start items-start sm:w-135 sm:pr-3 w-full">
+          <div className="flex flex-row justify-start items-start sm:w-135 sm:pr-3 w-full pl-2">
             <div className="flex flex-row gap-10 lg:gap-15 ">
               <Image
                 className="bg-white"
@@ -400,7 +564,7 @@ export default function Home() {
             </div>
           </div>
           {/* Basics of Disciplined Aglie */}
-          <div className="flex flex-row justify-start sm:w-135 pr-24 sm:pr-3 w-full ">
+          <div className="flex flex-row justify-start sm:w-135  sm:pr-3 w-full pl-2">
             <div className="flex flex-row gap-10 lg:gap-15 ">
               <Image
                 className="bg-white"
@@ -422,7 +586,10 @@ export default function Home() {
           </div>
         </div>
         {/* Education Section */}
-        <div className="w-full flex flex-col items-center gap-20 h-100 sm:h-200">
+        <div
+          id="education"
+          className="w-full flex flex-col items-center gap-20 h-100 sm:h-200 scroll-mt-12"
+        >
           <p className="text-3xl font-bold">Education</p>
           <Image
             className=""
